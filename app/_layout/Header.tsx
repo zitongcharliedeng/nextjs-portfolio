f4d07c468545bef6f01319e3ac0da24b0702a0b2
Zi-Tong <zitongcharliedeng2003@gmail.com>
2023-06-30 17:03:03 +0100

    Add linkedin icon svg to header

--- app/_layout/Header.tsx
+++ app/_layout/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react"
-import Link from "next/link"
 import { Button } from "@nextui-org/button"
 import HeaderTabs from "./_Header/HeaderTabs"
+import Image from "next/image"
 
 export default function Header({ router, darkMode, setDarkMode, }: { router: any, darkMode: boolean, setDarkMode: any }) {
     const handleThemeChange = () => {
@@ -9,10 +9,12 @@ export default function Header({ router, darkMode, setDarkMode, }: { router: any
     }
     return (
         <header>
-            <button onClick={() => console.log("h1")}>projects button test</button>
             <nav className="flex flex-row align-middle justify-between bg-gray-800 text-white px-4 py-2 rounded-t-lg">
                 <HeaderTabs router={router} />
-                <Button onPress={handleThemeChange}>🌞/🌑</Button>
+                <div className="flex flex-col">
+                    <Button onPress={handleThemeChange}>🌞/🌑</Button>
+                    <Image width={100} height={100} alt="LinkedIn Image" src="linkedin.svg" />
+                </div>
             </nav>
         </header>
     )
--- app/_layout/_Header/HeaderTabs.tsx
+++ app/_layout/_Header/HeaderTabs.tsx
@@ -13,18 +13,18 @@ export default function HeaderTabs({router}: {router:any}) {
             link: "",
             label: "Home",
             description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "The home page, a general about section/ summary of this site.",
         },
         {
             link: "projects",
             label: "Projects",
             description:
-                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "See my various coding projects. Maybe I'll add non-coding stuff too.",
         },
         {
             link: "contact",
             label: "Contact",
-            description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            description: "My contact details, please don't scam me!!!",
         },
     ]
 
