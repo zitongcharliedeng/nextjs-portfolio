09b3386d014c1aaf23ba4e285a516f1abeb758c8
Zi-Tong <zitongcharliedeng2003@gmail.com>
2023-07-03 15:27:47 +0100

    Reformat and style main to flex-grow

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import "./globals.scss"
 import Header from "./_layout/Header"
@@ -9,7 +9,7 @@ import { useRouter } from "next/navigation"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     const router = useRouter()
-    
+
     const [darkMode, setDarkMode] = React.useState(true)
     return (
         <html lang="en" className={darkMode ? "dark" : "light"}>
@@ -17,11 +17,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <title>the zcd portfolio</title>
             </head>
             {/* The body kinda looks like a floppy disk, maybe commit to this "intentional" style later... */}
-            <body className="bg-gradient-radial from-black to-white shadow-lg border-2 border-gray-300 rounded-lg">
+            <body>
                 <Providers>
-                    <Header router={router} darkMode={darkMode} setDarkMode={setDarkMode} />
-                    {children}
-                    <Footer />
+                    <div className="floppy-disk-1 flex flex-col bg-gradient-radial from-black to-white shadow-lg border-2 border-gray-300 rounded-lg">
+                        <Header router={router} darkMode={darkMode} setDarkMode={setDarkMode} />
+                        <main className="flex-grow overflow-y-auto">{children}</main>
+                        <Footer />
+                    </div>
                 </Providers>
             </body>
         </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-        Hello my name is Charlie. This is the home page of my nextjs
-        portfolio.
-      </h1>
-    </main>
-  );
+    return <h1>Hello my name is Charlie. This is the home page of my nextjs portfolio.</h1>
 }
