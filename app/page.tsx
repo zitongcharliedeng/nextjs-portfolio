2423677763001e306a8eb8f245dcf1937f5e4d33
Zi-Tong <zitongcharliedeng2003@gmail.com>
2023-07-04 17:06:03 +0100

    Realise that Tailwind has a built-in darkmode attr

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,42 @@
 import { Card, CardBody } from "@nextui-org/card"
+import { Spacer } from "@nextui-org/spacer"
 
 export default function Home() {
     return (
-        <div>
-            <Card isBlurred className="border-none notebook-lines">
-                <CardBody>
-                    Welcome
-                    <br />
-                    This is after a br tag.<p> this is after a p tAG.......AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-                    <a>link</a>
-                </CardBody>
-            </Card>
+        <div className="flex flex-col gap-4">
+            <div className="flex flex-row gap-4">
+                <Card className="border-none notebook-lines inline-block dark:bg-black dark:text-white">
+                    <CardBody className="p-notebook-line">
+                        <p>Welcome to my coding portfolio site.</p>
+                        <br />
+                        <p>Built in Next 13 and Tailwind.</p>
+                        <p>Enjoy!</p>
+                    </CardBody>
+                </Card>
+                <Card className="border-none notebook-lines inline-block">
+                    <CardBody className="p-notebook-line">
+                        Welcome.
+                        <br />
+                        This is my coding portfolio site.
+                        <br />
+                        Built in NextJS and Tailwind.
+                        <br />
+                        Enjoy!
+                    </CardBody>
+                </Card>
+            </div>
+            <div className="flex flex-row gap-4">
+                <Card className="border-none notebook-lines inline-block">
+                    <CardBody className="p-notebook-line">
+                        <b>TABLE OF CONTENTS</b>
+                        <br />
+                        <br />
+                        <ul>
+                            <li>- Sike go look at the header tabs box for summaries of each section.</li>
+                        </ul>
+                    </CardBody>
+                </Card>
+            </div>
         </div>
     )
 }
