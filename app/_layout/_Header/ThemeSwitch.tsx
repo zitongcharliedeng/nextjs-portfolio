dbec6d453c43fea2bac2cfe0617699d9bff73b50
Zi-Tong <zitongcharliedeng2003@gmail.com>
2023-07-18 12:40:24 +0100

    Implement nextuiv2 switch and system default theme

--- app/_layout/_Header/ThemeSwitch.tsx
+++ app/_layout/_Header/ThemeSwitch.tsx
@@ -1,9 +1,22 @@
 import { useState, useEffect } from "react"
 import { useTheme } from "next-themes"
+import {Switch} from "@nextui-org/react";
+import { SunIcon } from "./SunIcon";
+import { MoonIcon } from "./MoonIcon";
 
 const ThemeSwitch = () => {
+    const { systemTheme, setTheme } = useTheme()
+    const systemThemeIsDark = (systemTheme == "dark") ? true : false
+    const [isSelected, setIsSelected] = useState(systemThemeIsDark)
+    useEffect(() => {
+        if (isSelected == true) {
+            setTheme('dark')
+        } else {
+            setTheme('light')
+        }
+    }, [isSelected])
     const [mounted, setMounted] = useState(false)
-    const { theme, setTheme } = useTheme()
+    
 
     // useEffect only runs on the client, so now we can safely show the UI
     useEffect(() => {
@@ -14,12 +27,13 @@ const ThemeSwitch = () => {
         return null
     }
 
+    
+
     return (
-        <select value={theme} onChange={(e) => setTheme(e.target.value)}>
-            <option value="system">System</option>
-            <option value="dark">Dark🌑</option>
-            <option value="light">Light🌞</option>
-        </select>
+
+        <Switch defaultSelected size="md" color="success" startContent={<SunIcon />} endContent={<MoonIcon /> } isSelected={isSelected} onValueChange={setIsSelected}>
+        </Switch>
+
     )
 }
 
