68c82df02af4567891763ef00120f3453e983ea7
Zi-Tong <zitongcharliedeng2003@gmail.com>
2023-07-21 12:09:53 +0100

    Style image attachment popover to fit on screen

--- app/education/_page/AttachmentPopover.tsx
+++ app/education/_page/AttachmentPopover.tsx
@@ -2,23 +2,17 @@
 import { Popover, PopoverTrigger, PopoverContent, Button, Image } from "@nextui-org/react"
 import { CameraIcon } from "./CameraIcon"
 
-export default function AttachmentPopover() {
+export default function AttachmentPopover({src, alt}: {src: any, alt: any}) {
     return (
         <div className="flex flex-wrap gap-4">
             <Popover key={"blur"} showArrow offset={0} placement="left" backdrop={"blur"}>
                 <PopoverTrigger>
-                    <Button isIconOnly color="warning" variant="faded" className="photo-attachment">
+                    <Button isIconOnly color="warning" variant="faded" className="photo-attachment-button">
                         <CameraIcon />
                     </Button>
                 </PopoverTrigger>
-                <PopoverContent className="photo-attachment-tall-popover-content">
-                    <div className="px-2 py-2">
-                        <Image
-                            isZoomed
-                            alt="ocr-certificate-alevel"
-                            src="https://media.licdn.com/dms/image/C4E2DAQEj6rT553SOIA/profile-treasury-image-shrink_1280_1280/0/1677609079733?e=1690297200&v=beta&t=lrQdBHWCrEmd-ZIJUKDYLr0pPo8-Bi-JDScoVxdwO3I"
-                        />
-                    </div>
+                <PopoverContent className="photo-attachment-tall-popover-content px-2 py-2">
+                    <Image isZoomed alt={alt} src={src} />
                 </PopoverContent>
             </Popover>
         </div>
--- app/education/_page/CertificateTable.tsx
+++ app/education/_page/CertificateTable.tsx
@@ -16,28 +16,40 @@ export default function CertificateTable() {
                     <TableCell>Further Maths (+ Stats, Mech)</TableCell>
                     <TableCell>A (lost the * due to a misread DX)</TableCell>
                     <TableCell>
-                        <AttachmentPopover />
+                        <AttachmentPopover
+                            src="https://media.licdn.com/dms/image/C4E2DAQEj6rT553SOIA/profile-treasury-image-shrink_1280_1280/0/1677609079733?e=1690297200&v=beta&t=lrQdBHWCrEmd-ZIJUKDYLr0pPo8-Bi-JDScoVxdwO3I"
+                            alt=""
+                        />
                     </TableCell>
                 </TableRow>
                 <TableRow>
                     <TableCell>Maths (+ Stats, Mech)</TableCell>
                     <TableCell>A*</TableCell>
                     <TableCell>
-                        <AttachmentPopover />
+                        <AttachmentPopover
+                            src="https://media.licdn.com/dms/image/C4E2DAQEj6rT553SOIA/profile-treasury-image-shrink_1280_1280/0/1677609079733?e=1690297200&v=beta&t=lrQdBHWCrEmd-ZIJUKDYLr0pPo8-Bi-JDScoVxdwO3I"
+                            alt=""
+                        />
                     </TableCell>
                 </TableRow>
                 <TableRow>
                     <TableCell>Physics</TableCell>
                     <TableCell>A*</TableCell>
                     <TableCell>
-                        <AttachmentPopover />
+                        <AttachmentPopover
+                            src="https://media.licdn.com/dms/image/C4E2DAQEj6rT553SOIA/profile-treasury-image-shrink_1280_1280/0/1677609079733?e=1690297200&v=beta&t=lrQdBHWCrEmd-ZIJUKDYLr0pPo8-Bi-JDScoVxdwO3I"
+                            alt=""
+                        />
                     </TableCell>
                 </TableRow>
                 <TableRow>
                     <TableCell>Chemistry</TableCell>
                     <TableCell>A*</TableCell>
                     <TableCell>
-                        <AttachmentPopover />
+                        <AttachmentPopover
+                            src="https://media.licdn.com/dms/image/C4E2DAQHBTCvEXh87Yw/profile-treasury-image-shrink_1280_1280/0/1677609120971?e=1690534800&v=beta&t=Ufcw3RtL-fW2xhi-qe7aD4pV9tUa3EAsafbd8Po78Cs"
+                            alt=""
+                        />
                     </TableCell>
                 </TableRow>
             </TableBody>
